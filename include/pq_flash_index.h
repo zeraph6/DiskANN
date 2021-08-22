ca9a124ef91af84c2f10bbd9e7d35ff0549ebb16
Harsha Vardhan Simhadri <harshasi@microsoft.com>
2021-08-22 14:45:47 -0700

    DiskANN python bindings

--- include/pq_flash_index.h
+++ include/pq_flash_index.h
@@ -114,9 +114,8 @@ namespace diskann {
     DISKANN_DLLEXPORT void cached_beam_search(
         const T *query, const _u64 k_search, const _u64 l_search, _u64 *res_ids,
         float *res_dists, const _u64 beam_width, QueryStats *stats = nullptr);
-
-
-  DISKANN_DLLEXPORT _u32 range_search(const T *query1, const double range,
+    
+    DISKANN_DLLEXPORT _u32 range_search(const T *query1, const double range,
                                            const _u64 l_search, _u64* indices, float* distances,
                                            const _u64  beam_width,
                                            QueryStats *stats = nullptr);
